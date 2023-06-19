d1a5185736f762374ffe33bef4cef5375a94ed53
Dallas Palumbo <palumbodallas@gmail.com>
2023-06-19 10:24:11 -0400

    home

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br/>
+            <span className="orange_gradient text-center">AI-Powred Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam perferendis doloremque dicta illum id maxime dolores magni maiores quisquam quis ab molestiae, enim, eveniet asperiores non nostrum quam. Tempore, porro.
+        </p>
+        {/* feed */}
+    </section>
+  )
+}
+
+export default Home
